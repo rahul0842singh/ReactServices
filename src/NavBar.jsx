81f76e003ec16190af2fdf0b326a75470df569df
rahul0842singh <42551992+rahul0842singh@users.noreply.github.com>
2020-10-01 21:04:52 +0530

    Update NavBar.jsx

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -42,7 +42,7 @@ const NavBar = () => {
           <NavLink exact activeClassName="menu_active" className="nav-link" aria-current="page" to="/contact">Contact</NavLink>
         </li>
         <li className="nav-item">
-          <button className=" btn-danger btn-get-started scrollto ml-2"><AccessTimeIcon/> <Time/></button>
+         <button style={{color:"white"}} className=" btn-danger btn-get-started scrollto ml-2"><AccessTimeIcon/> <Time/></button>
         </li>
       
       
