95737e90ea3e9c99f9840bbe6e620d0d7e907cc5
rahul0842singh <rah0987654321@gmail.com>
2020-10-01 20:33:43 +0530

    Updates

---
+++ precache-manifest.ae0bc42e086f8a690e43c16f2a1d4b15.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6b02254235b30d6cd337ffae1304a4",
+    "url": "/ReactServices/index.html"
+  },
+  {
+    "revision": "18fdd4892031265f0d76",
+    "url": "/ReactServices/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "74bf2c123811d541982e",
+    "url": "/ReactServices/static/css/main.d212a6a3.chunk.css"
+  },
+  {
+    "revision": "18fdd4892031265f0d76",
+    "url": "/ReactServices/static/js/2.0a615084.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/ReactServices/static/js/2.0a615084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74bf2c123811d541982e",
+    "url": "/ReactServices/static/js/main.ee86b441.chunk.js"
+  },
+  {
+    "revision": "e64f9e852a7824c33aab",
+    "url": "/ReactServices/static/js/runtime-main.dafd1d45.js"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/ReactServices/static/media/about.619f0cc8.png"
+  },
+  {
+    "revision": "22925b944293d116eb83ad56fcb334f8",
+    "url": "/ReactServices/static/media/angular.22925b94.png"
+  },
+  {
+    "revision": "f702da2487a4da3e588c39b23a1d7985",
+    "url": "/ReactServices/static/media/hero.f702da24.svg"
+  },
+  {
+    "revision": "7362fa9d5d03bf05cd5f9d7ce32ee142",
+    "url": "/ReactServices/static/media/html.7362fa9d.jpg"
+  },
+  {
+    "revision": "d451f9a83622fb92b678a07d4dd49108",
+    "url": "/ReactServices/static/media/laravel.d451f9a8.png"
+  },
+  {
+    "revision": "e1eb58566a28676adf3ee7419956aef1",
+    "url": "/ReactServices/static/media/react.e1eb5856.png"
+  }
+]);
